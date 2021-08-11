943b024206ca80373a753fce6fe0e7dc4f71ac75
eojeda89 <59415294+eojeda89@users.noreply.github.com>
2021-08-11 11:08:46 -0400

    Initial commit

---
+++ src/main/java/com/eojeda/pronos/PronosApplication.java
@@ -0,0 +1,13 @@
+package com.eojeda.pronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PronosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PronosApplication.class, args);
+    }
+
+}
